b8b69a048318f4d0fa7b07b7166b8d6d97296d8c
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-08-12 03:28:14 +0200

    feat - add not found page

--- frontend/src/views/notFound/NotFound.vue
+++ frontend/src/views/notFound/NotFound.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-        <h1>Not Found</h1>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="font-bold text-5xl my-4 text-yellow-500">Page Not Found</h1>
+        <button class="bg-yellow-500 p-2 rounded font-bold my-4">
+            <router-link to="/">Go Back Home</router-link>
+        </button>
     </div>
 </template>
